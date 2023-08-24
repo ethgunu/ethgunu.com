22f5af8aa22af5ed3b0961b2737144f4a8f1a51f
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-08-24 22:01:14 +0200

    Fix build

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,12 +7,3 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
