1d09ce9bace89365d40660d748a1bca629a92871
kuzdogan <kuzdogan@users.noreply.github.com>
2023-08-24 20:11:13 +0000

    deploy: 757b1dc99f4b1ce893b9b1127e60c4226b649d11

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
