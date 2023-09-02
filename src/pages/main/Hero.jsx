9ac1a25a2b45e9355191594bc63d48fdbf74a6af
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-09-03 00:12:34 +0200

    Slogan section with countdown and rotation

---
+++ src/pages/main/Hero.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+import logo from "../../images/ethgunu-text-only.png"
+import devconnect from "../../images/devconnect.png"
+import { FaLocationDot } from "react-icons/fa6"
+import { BsArrowDownCircle } from "react-icons/bs"
+import "../../components/Kaleidoscope"
+import { Trans, Link } from "gatsby-plugin-react-i18next"
+
+export default function Hero() {
+  return (
+    <section className="flex flex-col justify-center h-screen min-w-screen bg-gradient-to-t from-gunuNavy from-0% to-40%">
+      <div class="flex-grow"></div>
+      <div className="flex flex-col items-center">
+        <img
+          src={logo}
+          alt="logo"
+          className="px-4 md:px-12 lg:w-[800px] lg:px-0"
+        />
+      </div>
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl md:text-4xl xl:text-5xl text-gunuOrange mt-2">
+          <Trans>Meet Ethereum</Trans>
+        </h1>
+      </div>
+      {/* Bottom elements */}
+      <div class="flex-grow"></div>
+      <div className="flex flex-row mx-16 mb-2 justify-between">
+        <div className="flex flex-row items-center">
+          <div className="text-5xl font-serif text-white">Nov 13 —</div>
+          <Link to="https://devconnect.org" target="_blank" rel="noopener">
+            <img src={devconnect} className="h-10 invert ml-4" />
+          </Link>
+        </div>
+        <Link
+          className="flex flex-row"
+          to="https://goo.gl/maps/N6VEtKT9xCMYMPk7A"
+          target="_blank"
+          rel="noopener"
+        >
+          <FaLocationDot className="text-gunuOrange text-xl mt-1 mr-2" />
+          <div className="text-white">
+            <div className="font-serif text-xl">
+              <Trans>Istanbul Congress Center</Trans>
+            </div>
+            <div className="text-xs">
+              Darülbedai Cad. No:3 34367 Şişli Maçka <br /> İstanbul/Türkiye
+            </div>
+          </div>
+        </Link>
+      </div>
+      {/* Scroll sign */}
+      <div className="flex flex-row mb-2 mx-16 items-center text-gray-200 opacity-40 animate-bounce uppercase">
+        <div className="text-sm">
+          <BsArrowDownCircle />
+        </div>
+        <div className="ml-4 text-xs">Scroll to continue</div>
+      </div>
+    </section>
+  )
+}
---
+++ src/pages/main/Slogan.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef, useState } from "react"
+import { motion, useScroll, useTransform } from "framer-motion"
+import blueTile from "../../images/tiles/tile-blue-small.png"
+import { Trans } from "gatsby-plugin-react-i18next"
+
+export default function Slogan() {
+  const targetRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "start start"],
+  })
+
+  console.log(scrollYProgress)
+  const rotation = useTransform(scrollYProgress, [0.2, 0.8], [0, 360])
+  const rotationClass = `rotate-[${rotation}]`
+
+  return (
+    <div className="flex flex-grow flex-col h-96 bg-gunuNavy px-36 text-center">
+      <div className="flex flex-col items-center justify-center my-12">
+        <motion.img
+          src={blueTile}
+          className={`w-20`}
+          style={{ objectFit: "contain", rotate: rotation }}
+          ref={targetRef}
+        />
+        <h2 className="text-5xl font-serif text-white mt-8">
+          <Trans components={{ underline: <span className="underline" /> }}>
+            learn-ethereum
+          </Trans>
+        </h2>
+        <Countdown
+          targetDateISO="2023-11-13T09:00:00"
+          className="mt-8 text-gunuLightBlue uppercase text-2xl"
+        />
+      </div>
+    </div>
+  )
+}
+
+const Countdown = ({ targetDateISO, className }) => {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
+  const calculateTimeLeft = () => {
+    const now = new Date()
+    const targetDate = new Date(targetDateISO)
+    const difference = targetDate.getTime() - now.getTime()
+
+    if (difference < 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 }
+    }
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
+    const minutes = Math.floor((difference / 1000 / 60) % 60)
+    const seconds = Math.floor((difference / 1000) % 60)
+
+    return { days, hours, minutes, seconds }
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+
+    return () => clearInterval(timerId)
+  }, [targetDateISO])
+
+  return (
+    <div className={className}>
+      <Trans
+        i18nKey="countdown"
+        values={{
+          days: timeLeft.days,
+          hours: timeLeft.hours,
+          minutes: timeLeft.minutes,
+          seconds: timeLeft.seconds,
+        }}
+        components={{ b: <strong /> }}
+      ></Trans>
+    </div>
+  )
+}
